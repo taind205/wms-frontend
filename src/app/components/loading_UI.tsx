d9808def4f5751c803ea199032c9e4001b4bb211
Tai Nguyen <taind205@gmail.com>
2023-11-05 18:18:17 +0700

    fix5

--- src/app/components/loading_UI.tsx
+++ src/app/components/loading_UI.tsx
@@ -3,8 +3,8 @@ import { Skeleton } from "@mui/material";
 
 export default function LoadingSkeleton1() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <></>
-  // <div className="w-screen h-screen" style={{backgroundColor:"#00000080"}}></div>
+  return <div className="w-screen h-screen" style={{backgroundColor:"#00000080"}}></div>
+  //<></>
   // <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rectangular" className="w-screen h-screen"/>
   // <main className="flex min-h-screen flex-col items-center justify-normal p-6">
   //   <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rectangular" width={800} height={560} />
